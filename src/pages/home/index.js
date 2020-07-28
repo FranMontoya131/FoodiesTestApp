dd151a7f272a0f1205f94a7d9aa503c300d45d9e
Javier Montoya <jmontoya@i-strategies.tech>
2020-07-28 03:10:22 -0600

    cambios en home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -13,17 +13,42 @@ class Home extends Component {
 
 	render(){
 		return(
-			<Imagen>
-				<img src="/img/logoYellow.svg" alt="logo"/>
-			</Imagen>
+			<>
+				<Imagen>
+					<img style={{ width: 550, height: 800}} src="/img/logoYellow.svg" alt="logo"/>
+				</Imagen>
+				<Container>
+					<Row style={{ justifyContent: 'space-between'}}>
+						<Col lg={4}>
+							<Row>
+								<Col lg={12}>
+									<p className='title-big'>Un nuevo sabor esta en la ciudad</p>
+								</Col>
+								<Col lg={12}>
+									<p className='text-basic'>Estamos a punto de descubrir un mundo lleno de sabores y de emociones inigualables</p>
+								</Col>
+								<Col lg={12}>
+									<p className='text-enlace'>Encuentranos</p>
+								</Col>
+							</Row>
+						</Col>
+						<Col lg={8}>
+							<img style={{ width: 567, height: 300 }} src="/img/hero_hamburger.svg" alt="hamburgesa"/>
+						</Col>
+					</Row>
+				</Container>
+			</>
 		);
 	}
 }
 
 export default Home;
 
-const Imagen = styled.span`
-	
+const Imagen = styled.div`
+	position: absolute;
+	top: -5em;
+	right: 0;
+	z-index: -10;
 
 	@media (max-width: 715px) {
 		/*cuando vaya cambiando el tamaño*/
